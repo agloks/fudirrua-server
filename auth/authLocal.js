a3d30edca66add276507d1061cf7cb1f77b3d52f
agloks <perdi9090@yahoo.com>
2019-10-24 18:57:46 -0300

    auth-local-done

---
+++ auth/authLocal.js
@@ -0,0 +1,26 @@
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/usersModel")
+
+module.exports = new LocalStrategy(
+  //modify username default for you desire
+  {
+    usernameField: "login"
+  },
+  //params is referent name of send in form
+  function(username, password, done) {
+    User.findOne({ login: username }, function (err, user) {
+      if (err) { console.log("here error strategy passport 0"); return done(err); }
+      if (!user) {
+        console.log("here error strategy passport 1")
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (user.password !== password) {
+        console.log("here error strategy passport 2")
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      console.log(user)
+      console.log("sucess LocalStrategy")
+      return done(null, user);
+    });
+  }
+);
