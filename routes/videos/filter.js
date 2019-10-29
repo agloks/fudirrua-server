6fd48a4ba2360f653687e6cbaaed75447bbb38ac
agloks <perdi9090@yahoo.com>
2019-10-29 18:18:41 -0300

    filter adjust

--- routes/videos/filter.js
+++ routes/videos/filter.js
@@ -15,7 +15,7 @@ router.get("/api/videos/home", async(req, res, next) => {
 
 // requisição de filtros personalizados
 router.post("/api/videos/filter", async(req, res, next) => {
-
+  console.log(req.body)
   let objSend = {}
   for (let item in req.body) {
     if( (item !== undefined) | item.length !== 0) {
@@ -24,9 +24,10 @@ router.post("/api/videos/filter", async(req, res, next) => {
   }
 
   try {
-    const recentsVideo = await Video.find(objSend).sort('-datePublication')
-    res.status(200).json(recentVideos)
+    const recentsVideo = await Video.find(objSend).sort('-datePublication').limit(20)
+    res.status(200).json(recentsVideo)
   } catch(err) {
+    console.log(err)
     res.status(500).json({error: err})
   }
 })
