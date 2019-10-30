ca189f1d69e5f8c8d4a63cd9b7ea9a7ad078f8ba
agloks <perdi9090@yahoo.com>
2019-10-29 21:37:35 -0300

    filter news condition

--- routes/videos/filter.js
+++ routes/videos/filter.js
@@ -18,12 +18,13 @@ router.post("/api/videos/filter", async(req, res, next) => {
   console.log(req.body)
   let objSend = {}
   for (let item in req.body) {
-    if( (item !== undefined) | item.length !== 0) {
+    if( ((item !== undefined) | item.length !== 0) & (req.body[item] !== "") ) {
       objSend[item] = req.body[item]
     }
   }
 
   try {
+    console.log(objSend)
     const recentsVideo = await Video.find(objSend).sort('-datePublication').limit(20)
     res.status(200).json(recentsVideo)
   } catch(err) {
