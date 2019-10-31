eebdd30a7566b527a824d350e32831d56a09dbcf
agloks <perdi9090@yahoo.com>
2019-10-31 12:16:54 -0300

    adjust history

--- routes/videos/history.js
+++ routes/videos/history.js
@@ -1,22 +1,24 @@
 const router = require("../index")
 const Video = require("../../models/videosModel")
 const User = require("../../models/usersModel")
+const passport = require("passport")
 
 //salvar o histórico de video do usuario, e de usários que viu o video
-router.get("/api/videos/history/save/:videoId", async (req, res, next) => {
-  const {videoId} = req.params
+router.get("/api/videos/history/save/:videoId/:idUser", async (req, res, next) => {
+  const {videoId, idUser} = req.params
   try {
-    if(req.isAuthenticated()) {
-      const userId = req.user._id
-      await User.findByIdAndUpdate(userId, {$push: {historyVideos: videoId}})
-      await Video.findByIdAndUpdate(videoId, {$push: {views: userId}})
+    if( (idUser !== undefined | idUser !== null) ) {
+      const VideoFound = await Video.findOneAndUpdate({idYoutubeVideo:videoId})
+      await User.findByIdAndUpdate(idUser, {$push: {historyVideos: VideoFound._id}})
+      await Video.findOneAndUpdate({idYoutubeVideo:videoId}, {$push: {views: idUser}})
     } else {
-      const VideoFound = await Video.findById(videoId)
+      const VideoFound = await Video.findOneAndUpdate({idYoutubeVideo:videoId})
       let numberViewsAnonymos = VideoFound.viewsAnonymous + 1
-      await Video.findByIdAndUpdate(videoId, {viewsAnonymous: numberViewsAnonymos})
+      await Video.findOneAndUpdate({idYoutubeVideo:videoId}, {viewsAnonymous: numberViewsAnonymos})
     }
     res.status(200).json({sucess: "sucesso ao salvar history"})
   } catch(err) {
+    console.log(err)
     res.status(500).json({error: `error ao salva history \n ${err}`})
   }
 })
