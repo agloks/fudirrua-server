4ae5ffc4f7c68fdd207383313435392b0f2be636
agloks <perdi9090@yahoo.com>
2019-10-31 17:17:03 -0300

    await

--- bin/youtube.js
+++ bin/youtube.js
@@ -97,7 +97,7 @@ class YoutubeApiVideosPlaylist {
 }
 
 async function genVideoDB(ID, qnt) {
-  // await mongoose
+  await mongoose
   .connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true })
   .then(x => { console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)})
   .catch(err => { console.error('Error connecting to mongo', err)});
